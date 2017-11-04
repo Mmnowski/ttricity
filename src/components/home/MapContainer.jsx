f8a8056165df91dcf6c47acd87cb261185c7b297
msojka96 <mikolaj.sojka@gmail.com>
2017-11-04 19:03:43 +0100

    Map Third version & descriptions.

--- src/components/home/MapContainer.jsx
+++ src/components/home/MapContainer.jsx
@@ -4,48 +4,68 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import places from '../firebaseData.js';
 
 
-class MapContainer extends React.Component {
+export class MapContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+        }
 
-    renderHome() {
+        // binding this to event-handler functions
+        this.onMarkerClick = this.onMarkerClick.bind(this);
+    }
+
+    onMarkerClick = (props, marker, e) => {
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+    }
+
+
+
+    render(){
         const style = { display: 'inline-block', margin: '0 16% 0 0', height:'600px' }
 
         return (
+            <Map style={style} google={this.props.google}
+                 initialCenter={{
+                     lat: 54.5039043,
+                     lng: 18.3934396
+                 }}
+                 zoom={10}
+            >
 
-            <div >
 
-                <Map style={style} google={this.props.google}
-                     initialCenter={{
-                         lat: 54.5039043,
-                         lng: 18.3934396
-                     }}
-                     zoom={10}
-                >
+            <Marker name={'Muzeum miasta Gdyni'} onClick={this.onMarkerClick}
+                        position={{lat: 54.515825, lng: 18.5449753}} />
 
-                    <Marker name={'Sea Towers'}
-                            position={{lat: 54.5222336, lng: 18.5515414}} >
 
-                    </Marker>
+                <Marker name={'Sea Towers'} onClick={this.onMarkerClick}
+                        position={{lat: 54.5222336, lng: 18.5515414}} />
 
-                    <Marker name={'Muzeum miasta Gdyni'}
-                            position={{lat: 54.515825, lng: 18.5449753}}>
-                    </Marker>
 
-                    <Marker name={'Okręt wojskowy ORP Błyskawica'}
-                            position={{lat: 54.5195615, lng: 18.5489877}}>
-                    </Marker>
 
-                </Map>
-            </div>
+                <Marker name={'Okręt wojskowy ORP Błyskawica'} onClick={this.onMarkerClick}
+                        position={{lat: 54.5195615, lng: 18.5489877}} />
 
-        );
-    };
 
-    render() {
-        return this.renderHome();
+
+
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
+            </Map>
+        )
     }
 }
-
-//export default MapContainer;
 
 
 export default GoogleApiWrapper({
