5575fa1ff91e0bd76bdfc946ca34e98295e8adf2
Mmnowski <pawel.stankiewicz.1996@gmail.com>
2017-11-06 16:53:33 +0100

    href routing on navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './layout.scss';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
 class NavBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {open: true};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {open: true};
+  }
 
-    handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle = () => this.setState({open: !this.state.open});
 
-    render() {
-        return (
-            <Drawer open={this.state.open} containerStyle={{width: '17.6%', top: 8}}>
-              <MenuItem>Strona startowa</MenuItem>
-              <MenuItem>Lista atrakcji</MenuItem>
-            </Drawer>
-        );
-    }
+  render() {
+    return (
+      <Drawer open={this.state.open} containerStyle={{width: '17.6%', top: 8}}>
+        {/*  <MenuItem
+          containerElement={<Link to="/" />}
+          primaryText="Test"
+        />
+        <MenuItem
+          containerElement={<Link to="/lista" />}
+          primaryText="Test2"
+        />
+       */}
+        <MenuItem
+          href="/#/"
+          primaryText="Strona glowna"
+        />
+        <MenuItem
+          href="/#/lista"
+          primaryText="Lista atrakcji"
+        />
+      </Drawer>
+    );
+  }
 }
 
 export default NavBar;
