24ef5fab71b8b4e650e5c76a0cc568d6f056cf9b
Arkadiusz Klein <arek.klein@limebrains.com>
2017-12-04 20:47:49 +0100

    sample fetch user location

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -5,7 +5,7 @@ export const ROOT_URL = config.apiRootUrl;
 export const API_ACTIONS = {
   SAMPLE_ACTION: {
     action: 'sample_action',
-    url: `${ROOT_URL}auth/convert-token/`,
+    url: `https://www.googleapis.com/geolocation/v1/geolocate`,
     startRequest: 'sample_action/REQUEST_STARTED',
     successRequest: 'sample_action/REQUEST_SUCCESS',
     failureRequest: 'sample_action/REQUEST_FAILURE',
--- src/redux/modules/cardlist/actions.js
+++ src/redux/modules/cardlist/actions.js
@@ -1,8 +1,17 @@
 import {API_ACTIONS} from '../../actionTypes';
+import {startRequest} from '../../api';
 
 export function selectPlace(place) {
   return {
     payload: {place},
     type: API_ACTIONS.SELECT_PLACE
   };
+}
+
+export function test() {
+  const action = API_ACTIONS.SAMPLE_ACTION;
+  const postData = {};
+  const attrs = {};
+  const params = {key: 'GOOGLE_API_KEY'};
+  return startRequest({}, action, attrs, params, 'POST', postData);
 }
