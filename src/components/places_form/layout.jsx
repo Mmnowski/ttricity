102cc0dff1c8beecd460c9196e5e5853dc83bd7b
akane <arek.klein@gmail.com>
2018-01-08 17:15:39 +0100

    fix for unauth user

--- src/components/places_form/layout.jsx
+++ src/components/places_form/layout.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import {connect} from 'react-redux';
 import {TextField} from "material-ui";
+import {history} from '../../prepare';
 import {createPlace} from "../../redux/modules/places_form/actions";
 
 class PlacesForm extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       desc: "",
@@ -22,38 +23,44 @@ class PlacesForm extends React.Component {
     }
   }
 
-  sendPlace() {
-    let check = 0;
-    const {desc, img, lat, lon, name} = this.state;
-
-    if(desc === ""){
-      check++;
-      this.setState({descErrorText: "To pole jest obowiazkowe"});
-    }
-    if(img === ""){
-      check++;
-      this.setState({imgErrorText: "To pole jest obowiazkowe"});
-    }
-    if(lat === ""){
-      check++;
-      this.setState({latErrorText: "To pole jest obowiazkowe"});
-    }
-    if(lon === ""){
-      check++;
-      this.setState({lonErrorText: "To pole jest obowiazkowe"});
-    }
-    if(name === "") {
-      check++;
-      this.setState({nameErrorText: "To pole jest obowiazkowe"});
-    }
-    if(check === 0){
-      this.props.createPlace(desc, img, lat, lon, name);
-      this.setState({desc: "", img: "", lat: "", lon: "", name: ""});
+  componentWillMount() {
+    if (!this.props.user || !this.props.admins || !this.props.admins[this.props.user.uid]) {
+      history.push('/');
     }
   }
 
-  render(){
-    return(
+  sendPlace = () => {
+    let check = 0;
+    const {desc, img, lat, lon, name} = this.state;
+
+    if (desc === "") {
+      check++;
+      this.setState({descErrorText: "To pole jest obowiazkowe"});
+    }
+    if (img === "") {
+      check++;
+      this.setState({imgErrorText: "To pole jest obowiazkowe"});
+    }
+    if (lat === "") {
+      check++;
+      this.setState({latErrorText: "To pole jest obowiazkowe"});
+    }
+    if (lon === "") {
+      check++;
+      this.setState({lonErrorText: "To pole jest obowiazkowe"});
+    }
+    if (name === "") {
+      check++;
+      this.setState({nameErrorText: "To pole jest obowiazkowe"});
+    }
+    if (check === 0) {
+      this.props.createPlace(desc, img, lat, lon, name);
+      this.setState({desc: "", img: "", lat: "", lon: "", name: ""});
+    }
+  };
+
+  render() {
+    return (
       <div className="formBox">
         <div className="field">
           <TextField
@@ -65,13 +72,13 @@ class PlacesForm extends React.Component {
           />
         </div>
         <div className="field">
-        <TextField
-          name="img"
-          value={this.state.img}
-          hintText="Link do zjęcia"
-          errorText={this.state.imgErrorText}
-          onChange={(e) => this.setState({img: e.target.value, imgErrorText: ""})}
-        />
+          <TextField
+            name="img"
+            value={this.state.img}
+            hintText="Link do zjęcia"
+            errorText={this.state.imgErrorText}
+            onChange={(e) => this.setState({img: e.target.value, imgErrorText: ""})}
+          />
         </div>
         <div className="field">
           <TextField
@@ -83,13 +90,13 @@ class PlacesForm extends React.Component {
           />
         </div>
         <div className="field">
-        <TextField
-          name="lon"
-          value={this.state.lon}
-          hintText="Lon"
-          errorText={this.state.lonErrorText}
-          onChange={(e) => this.setState({lon: e.target.value, lonErrorText: ""})}
-        />
+          <TextField
+            name="lon"
+            value={this.state.lon}
+            hintText="Lon"
+            errorText={this.state.lonErrorText}
+            onChange={(e) => this.setState({lon: e.target.value, lonErrorText: ""})}
+          />
         </div>
         <div className="field">
           <TextField
@@ -101,16 +108,22 @@ class PlacesForm extends React.Component {
           />
         </div>
         <div className="sendButton">
-        <FlatButton className="cardButton" label="Dodaj" onClick={() => {
-          this.sendPlace();}}/>
+          <FlatButton className="cardButton" label="Dodaj" onClick={this.sendPlace}/>
         </div>
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    user: state.auth.user,
+    admins: state.auth.admins,
+  };
+}
+
 const mapDispatchToProps = {
   createPlace,
 };
 
-export default connect(null,mapDispatchToProps)(PlacesForm);
+export default connect(mapStateToProps, mapDispatchToProps)(PlacesForm);
