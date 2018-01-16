cad53094786a09e87b7bce73331fbe94fcbd565e
Mmnowski <pawel.stankiewicz.1996@gmail.com>
2018-01-17 00:42:43 +0100

    major css refactor

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,8 +7,6 @@ import {fetchPlaces} from '../redux/modules/map/actions';
 import {fetchComments, fetchRatings} from '../redux/modules/cardlist/actions';
 import {GoogleApiWrapper} from 'google-maps-react';
 
-let MediaQuery = require('react-responsive');
-
 class Layout extends React.Component {
   constructor(props) {
     super(props);
@@ -29,37 +27,10 @@ class Layout extends React.Component {
     }
   }
 
-  mediaCheck() {
-    return (
-      <MediaQuery minDeviceWidth={1300}>
-        {(matches) => {
-          if (matches) {
-            if (this.state.mobile) {
-              this.setState({mobile: false});
-            }
-          } else {
-            if (!this.state.mobile) {
-              this.setState({mobile: true});
-            }
-          }
-          return (null);
-        }}
-      </MediaQuery>
-    );
-  };
-
   render() {
     // _.map(places, (place) => console.log(place));
     let children = this.props.children;
     let shouldOpen = this.props.open;
-    // if (!this.props.isLogged) {
-    //   if (children.props.location.pathname !== '/') {
-    //     history.replace('/');
-    //     children = null;
-    //   }
-    //   shouldOpen = false;
-    // }
-    let navWidth = shouldOpen && !this.state.mobile ? '88.5%' : '100%';
     return (
       <div className="main-container">
         <section className="content-container">
