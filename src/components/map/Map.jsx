16cd0e3d56e36ea2a65dd9e6e392eab080bf9e72
Arkadiusz Klein <arek.klein@gmail.com>
2018-05-19 17:08:10 +0200

    Revert "styled marker popup a bit but still needs a proper positioning"
    This reverts commit 0ca4b16a0a252f90c1ece38dbb14066725a4ed8b.

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -10,66 +10,22 @@ export class Map extends React.Component {
     super(props);
     this.state = {
       activeMarker: null,
-      center: {
-        lat: 54.439444,
-        lng: 18.5721789,
-      },
-      zoom: 11,
     }
   }
 
   componentWillMount() {
     const {selectedPlace} = this.props;
     if (selectedPlace) {
-      const newCenter = {
-        lat: parseFloat(selectedPlace.lat),
-        lng: parseFloat(selectedPlace.lon),
-      };
-      this.setState({activeMarker: selectedPlace, center: newCenter, zoom: 14});
-      this.props.selectPlace(null);
+      this.setState({activeMarker: selectedPlace});
     }
   }
 
   manageActive = (place) => this.setState({activeMarker: place});
 
   renderMarkerPopup(place) {
-    const {activeMarker} = this.state;
-    place = activeMarker;
-    const markerPopup = document.getElementById('marker-popup');
-    console.log(markerPopup);
-    if (markerPopup) {
-      console.log(markerPopup.clientHeight);
-      console.log(markerPopup.clientWidth);
-    }
-    let currentDisplay = 'block';
-    if (!place.display) {
-      place = {...place, display: 'block'};
-      setTimeout(() => this.setState({activeMarker: place}), 0);
-    }
-    else {
-      currentDisplay = 'block';
-    }
     return (
-      <div id="marker-popup" style={{display: currentDisplay}}>
-        <div className='title'>
-          {place.name}
-          <span className="close" onClick={this.manageActive}>
-            x
-          </span>
-        </div>
-        <div className='description'>
-          <div className='photo'>
-            <img
-              alt="obrazek"
-              src={place.img}
-              width={'100%'}
-              height={'100%'}
-            />
-          </div>
-          <div className='text'>
-            {place.description}
-          </div>
-        </div>
+      <div>
+        {place.name}
       </div>
     )
   }
@@ -87,13 +43,12 @@ export class Map extends React.Component {
         {isActive && this.renderMarkerPopup(place)}
         <img
           alt="marker"
-          className="marker"
           src="imgs/marker.png"
           onClick={() => this.manageActive(place)}
         />
       </div>
     );
-  };
+  }
 
   render() {
     const {places} = this.props;
@@ -113,15 +68,17 @@ export class Map extends React.Component {
         />
       );
     }
-    const {center, zoom} = this.state;
     const markers = _.map(places, this.renderMarker);
     return (
       <div style={{width: '100vw', height: '100vh', position: 'absolute', top: 0, left: 0}}>
         <GoogleMapReact
           // apiKey={null}
           bootstrapURLKeys={{key: process.env.MAP_KEY}}
-          center={center}
-          zoom={zoom}
+          center={{
+            lat: 54.439444,
+            lng: 18.5721789,
+          }}
+          zoom={11}
         >
           {markers}
         </GoogleMapReact>
